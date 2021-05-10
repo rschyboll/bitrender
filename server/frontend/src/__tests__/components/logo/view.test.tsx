fb227bdf7ebac4eb8ffe7f21269c51111ec7def6
Robin Schyboll <Robin.Schyboll@gmail.com>
2021-05-10 15:25:27 +0200

    (fix) Fixed logo test to use only view

--- server/frontend/src/__tests__/components/logo/view.test.tsx
+++ server/frontend/src/__tests__/components/logo/view.test.tsx
@@ -1,22 +1,22 @@
 import { render, screen } from "@testing-library/react";
 
-import { Logo } from "../../../components/logo";
+import { LogoView } from "../../../components/logo/view";
 
 test("Renders without errors", () => {
-  render(<Logo />);
+  render(<LogoView />);
   const logoElement = screen.getByRole("img");
   expect(logoElement).toBeInTheDocument();
 });
 
 test("Has src and alt attributes", () => {
-  render(<Logo />);
+  render(<LogoView />);
   const logoElement = screen.getByRole("img");
   expect(logoElement).toHaveAttribute("src", "logo.png");
   expect(logoElement).toHaveAttribute("alt", "logo");
 });
 
 test("Has class and style when providing props", () => {
-  render(<Logo className="test" style={{ backgroundColor: "red" }} />);
+  render(<LogoView className="test" style={{ backgroundColor: "red" }} />);
   const logoElement = screen.getByRole("img");
   expect(logoElement).toHaveClass("test");
   expect(logoElement).toHaveStyle("background-color: red");
