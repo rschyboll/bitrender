a821131e115530ac86f6c8f5be269642bc3bdc30
Robin Schyboll <Robin.Schyboll@gmail.com>
2021-05-10 15:50:16 +0200

    (test) Added tests for topbar

---
+++ server/frontend/src/__tests__/components/topbar/view.test.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+
+import { TopbarView } from "../../../components/topbar/view";
+
+jest.mock("react-i18next", () => ({
+  useTranslation: () => {
+    return {
+      t: (str: string) => str,
+    };
+  },
+}));
+
+test("Renders without errors", () => {
+  const location = "test";
+  render(<TopbarView location={location} />);
+  const logoElement = screen.getByText("navigation." + location);
+  expect(logoElement).toBeInTheDocument();
+});
