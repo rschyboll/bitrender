f74d3c383c0da4279feececdda27691a3eeeb441
Robin Schyboll <robin.schyboll@gmail.com>
2022-05-11 19:29:52 +0200

    (wip) Acl helper tests

--- backend/tests/unit/auth/test_acl_helper.py
+++ backend/tests/unit/auth/test_acl_helper.py
@@ -1,12 +1,31 @@
-from bitrender.auth.acl import AclList
-from bitrender.auth.acl_helper import AclHelper
-from bitrender.tests.
+from unittest.mock import MagicMock
 
-async def test_sacl_simple_success():
+from pytest_mock import MockerFixture
+
+from bitrender.auth.acl import AclAction, AclList, AclPermit
+from bitrender.auth.acl_helper import AclHelper
+from tests.unit.models import ExampleModel
+
+
+def test_sacl_simple_success(mocker: MockerFixture):
     """Tests the get_latest method with lock set to False."""
-    model_acl = [()]
-    model_dacl = [()]
-    user_acl_ids = [()]
+    acl_action = AclAction.CREATE
+    model_sacl: AclList = [(AclPermit.ALLOW, "user:test", AclAction.CREATE)]
+    user_acl_ids = [("user:test")]
+    __test_sacl(user_acl_ids, model_sacl, mocker, acl_action, True)
+
+
+def __test_sacl(
+    auth_ids: list[str], sacl: AclList, mocker: MockerFixture, action: AclAction, result: bool
+):
+    mock_sacl = MagicMock(return_value=sacl)
+    mocker.patch("tests.unit.models.ExampleModel.__sacl__", new=mock_sacl)
+    acl_helper = AclHelper(auth_ids)
+    assert acl_helper.static_check([ExampleModel], action) == result
+
+
+def __mock_sacl(sacl: AclList):
+    mock = MagicMock(return_value=sacl)
 
 
 async def test_sacl_multiple_models_success():
@@ -19,4 +38,3 @@ async def test_sacl_multiple_actions_failure():
 
 async def __test_acl_helper(auth_ids: list[str], sacl: AclList, dacl: list[AclList]):
     acl_helper = AclHelper(auth_ids)
-    
